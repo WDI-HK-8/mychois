1eeea477191c5af15c5a3e424504bb3332e91bc2
Rachel <rachella@gmail.comOB>
2015-08-22 17:56:45 +0800

    feat(users)- Add user type to user table & create model relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  belongs_to :district
+  belongs_to :user_type
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable,
