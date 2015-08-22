69fecedc28db969898da0476c7efca8558c74256
Rachel <rachella@gmail.comOB>
2015-08-22 15:52:48 +0800

    feat(users)- Add district ref to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150822072454) do
+ActiveRecord::Schema.define(version: 20150822074858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,10 +42,13 @@ ActiveRecord::Schema.define(version: 20150822072454) do
     t.json     "tokens"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "district_id"
   end
 
+  add_index "users", ["district_id"], name: "index_users_on_district_id", using: :btree
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true, using: :btree
 
+  add_foreign_key "users", "districts"
 end
