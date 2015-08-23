670d9b1d3ed965f9aa6db7539fd92ae64a737242
teelu <terrence.lun@gmail.com>
2015-08-23 14:05:01 +0800

    fix(users) - Paths are now /api/v1/pathname

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  mount_devise_token_auth_for 'User', at: 'api/v1/auth'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'static_pages#index'
 
-  resources 'districts'
+  resources 'districts', :path => 'api/v1/districts'
 
-  resources 'user_types'
+  resources 'user_types', :path => 'api/v1/usertypes'
 
 end
