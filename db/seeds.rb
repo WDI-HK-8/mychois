b6f51819439dd83877aa316b140aa26f16eefddb
teelu <terrence.lun@gmail.com>
2015-08-23 15:25:12 +0800

    feat(users) - Added db seeds for user data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+districts = ['Central',
+             'Wan Chai',    
+             'Sham Shui Po',
+             'Kowloon City',
+             'Kwun Tong',
+             'Wong Tai Sin', 
+             'Yau Tsim Mong',
+             'Kwai Tsing',
+             'North Point',
+             'Sai Kung',
+             'Sha Tin',
+             'Tai Po',
+             'Tsuen Wan', 
+             'Tuen Mun',
+             'Yuen Long'
+            ]
+
+usertypes = ['Shopper',
+             'Customer'
+            ]
+districts.each do |district|
+  District.where(name: district).first_or_create(name: district)
+end
+
+usertypes.each do |usertype|
+  UserType.where(name: usertype).first_or_create(name: usertype)
+end
