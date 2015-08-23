ca37c16bcccc07800db4a582836b08197ce3b7ee
teelu <terrence.lun@gmail.com>
2015-08-23 15:42:36 +0800

    fix(users) - rebase

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   belongs_to :district
   belongs_to :user_type
+
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable,
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20150822094819) do
 
   # These are extensions that must be enabled in order to support this database
@@ -27,7 +28,6 @@ ActiveRecord::Schema.define(version: 20150822094819) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
   create_table "users", force: :cascade do |t|
     t.string   "provider",               default: "email", null: false
     t.string   "uid",                    default: "",      null: false
