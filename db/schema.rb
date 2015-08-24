eb09d7e5d123e636c1efc7184824ee8bea73ed50
Rachel <rachella@gmail.comOB>
2015-08-24 14:23:01 +0800

    fix(users)- schema fix from rebase

--- db/schema.rb
+++ db/schema.rb
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20150822080143) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "district_id"
+    t.integer  "user_type_id"
   end
 
   add_index "users", ["district_id"], name: "index_users_on_district_id", using: :btree
@@ -58,4 +59,5 @@ ActiveRecord::Schema.define(version: 20150822080143) do
   add_index "users", ["user_type_id"], name: "index_users_on_user_type_id", using: :btree
 
   add_foreign_key "users", "districts"
+  add_foreign_key "users", "user_types"
 end
