8d67f80bd8a3eebc8995563ff41230a524c8f4ad
Rachel <rachella@gmail.comOB>
2015-08-25 15:57:57 +0800

    enhance(users_frontend)- Change user sign up route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'Worker', at: 'api/v1/workers'
+  mount_devise_token_auth_for 'User', at: 'api/v1/auth'
   mount_devise_token_auth_for 'Customer', at: 'api/v1/customers'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
