275b2716cb2eaacb58a28986acf386c9ea6e2a2c
Rachel <rachella@gmail.comOB>
2015-08-26 14:54:56 +0800

    feat(groceries-list-update)- Update grocery lists

--- app/controllers/grocery_lists_controller.rb
+++ app/controllers/grocery_lists_controller.rb
@@ -1,10 +1,22 @@
 class GroceryListsController < ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   def create
     @grocery_list = GroceryList.new(grocery_list_params)
     @grocery_list[:customer_id] = current_user.id
     @grocery_list.save
     render json: @grocery_list
+  end
+
+  def update
+    @grocery_list = GroceryList.find_by_id(params[:id])
+
+    if @grocery_list.nil?
+      render #message can't find it
+
+    else
+      @grocery_list.update(grocery_list_params)
+
+    end
   end
 
   def grocery_list_params
---
+++ app/views/grocery_lists/update.jbuilder
@@ -0,0 +1,3 @@
+json.id                   @grocery_list.id
+json.additional_comments  @grocery_list.additional_comments
+json.substitutions        @grocery_list.substitutions
