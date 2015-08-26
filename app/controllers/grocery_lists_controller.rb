de1f9ec12efc85026fbfc292ebd72238f9972d00
teelu <terrence.lun@gmail.com>
2015-08-26 13:48:49 +0800

    feat(grocery-list-index) - finish index backend start front end

--- app/controllers/grocery_lists_controller.rb
+++ app/controllers/grocery_lists_controller.rb
@@ -1,10 +1,14 @@
 class GroceryListsController < ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   def create
     @grocery_list = GroceryList.new(grocery_list_params)
     @grocery_list[:customer_id] = current_user.id
     @grocery_list.save
     render json: @grocery_list
+  end
+
+  def index
+    @grocery_lists = GroceryList.all
   end
 
   def grocery_list_params
---
+++ app/views/grocery_lists/index.jbuilder
@@ -0,0 +1,6 @@
+json.array! @grocery_lists do |grocery_list|
+  json.id     grocery_list.id
+  json.customer_id  grocery_list.customer_id
+  json.comments   grocery_list.additional_comments
+  json.substitutions   grocery_list.substitutions
+end
