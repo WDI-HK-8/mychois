2cb7826ffb7343511e294aac60ee15813796109d
teelu <terrence.lun@gmail.com>
2015-08-26 21:49:30 +0800

    feat(groceries-list-delete) - delete complete

--- app/controllers/grocery_lists_controller.rb
+++ app/controllers/grocery_lists_controller.rb
@@ -1,5 +1,5 @@
 class GroceryListsController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
   def create
     @grocery_list = GroceryList.new(grocery_list_params)
     @grocery_list[:customer_id] = current_user.id
@@ -9,12 +9,19 @@ class GroceryListsController < ApplicationController
 
   def index
     @grocery_lists = GroceryList.where(:customer_id => current_user.id)
-  end 
+  end  
 
-  def delete
-    @grocery_list = GroceryList.find_by_id(:id)
-    render json: {error: "No record found"}, :status => :bad_request if @grocery_lists.nil? 
-    
+  def destroy
+    @grocery_list = GroceryList.find_by_id(params[:id])
+    if @grocery_list.nil? 
+      render json: {error: "No record found"}, :status => :bad_request
+    else
+      if current_user.id == @grocery_list.customer_id
+        @grocery_list.destroy
+      else
+        render json: {error: "Unauthorized"}, :status => :unauthorized
+      end
+    end
   end
   private
     def grocery_list_params
