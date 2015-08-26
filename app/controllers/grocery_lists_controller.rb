bd8aa7c0bdaac85ca6fa01a67700af9437eb866b
teelu <terrence.lun@gmail.com>
2015-08-26 17:17:11 +0800

    feat(groceries-list-delete) - delete controller initiated

--- app/controllers/grocery_lists_controller.rb
+++ app/controllers/grocery_lists_controller.rb
@@ -1,5 +1,5 @@
 class GroceryListsController < ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   def create
     @grocery_list = GroceryList.new(grocery_list_params)
     @grocery_list[:customer_id] = current_user.id
@@ -11,7 +11,14 @@ class GroceryListsController < ApplicationController
     @grocery_lists = GroceryList.where(:customer_id => current_user.id)
   end 
 
-  def grocery_list_params
-    params.require(:grocery_list).permit(:additional_comments, :substitutions, :name)
+  def delete
+    @grocery_list = GroceryList.find_by_id(:id)
+    render json: {error: "No record found"}, :status => :bad_request if @grocery_lists.nil? 
+    
   end
+  private
+    def grocery_list_params
+      params.require(:grocery_list).permit(:additional_comments, :substitutions, :name)
+    end
+
 end
