628c40be264b5406423823fcfbabde9127416cad
teelu <terrence.lun@gmail.com>
2015-08-27 10:39:09 +0800

    feat(grocery-list-item-create) - create list items done

--- app/controllers/grocery_list_items_controller.rb
+++ app/controllers/grocery_list_items_controller.rb
@@ -1,7 +1,11 @@
 class GroceryListItemsController < ApplicationController
   def create
     grocery_list_id = params[:grocery_list_id]
-    @grocery_list_item = GroceryListItem.new(grocery_list_item_params)
+    grocery_list = grocery_list_item_params
+    grocery_list[:grocery_id] = Grocery.find_by(name: grocery_list[:grocery_name]).id
+    grocery_list.delete(:grocery_name)
+
+    @grocery_list_item = GroceryListItem.new(grocery_list)
     @grocery_list_item.grocery_list_id = grocery_list_id
     if @grocery_list_item.grocery.sale_price.nil?
       @grocery_list_item.current_price = @grocery_list_item.grocery.price
@@ -21,6 +25,6 @@ class GroceryListItemsController < ApplicationController
 
   private
     def grocery_list_item_params
-      params.require(:grocery_list_item).permit(:quantity, :grocery_id)
+      params.require(:grocery_list_item).permit(:quantity, :grocery_name)
     end
 end
